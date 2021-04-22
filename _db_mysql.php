7fcd725f017a952024e415d64a23287c9ce94114
Hanjiang Yu <yuhanjiang0417@gmail.com>
2021-04-22 15:30:58 +0800

    fix

--- _db_mysql.php
+++ _db_mysql.php
@@ -5,9 +5,9 @@ $password = "bmgt407_2021s_07";
 $dbname = "bmgt407_2021s_07_db";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
