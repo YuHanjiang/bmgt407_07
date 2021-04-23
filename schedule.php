1d0d2d829bbcc3cf5090747c4b31287bc2bc12d0
Hanjiang Yu <yuhanjiang0417@gmail.com>
2021-04-23 15:14:05 +0800

    debug

--- schedule.php
+++ schedule.php
@@ -9,7 +9,7 @@ $courses = getRows("SELECT courseName FROM course");
 $tutors = getRows("SELECT firstName, lastName, email from users where accountType = 1");
 if (isset($_POST['courseSelect']) and $_SESSION['courseName'] = 'UNDEFINED') {
     $_SESSION['courseName'] = $_POST['course'];
-    $courseName = $_SESSION['courseName'];
+    $courseName = $_POST['course'][0];
     echo "<script>alert('$_SESSION]')</script>";
 } else if (isset($_POST['courseReselect'])) {
     $_SESSION['courseName'] = 'UNDEFINED';
