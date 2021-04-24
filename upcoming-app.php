4e0201b7d5da594b3cc34733a1c8cd71ec075652
Hanjiang Yu <yuhanjiang0417@gmail.com>
2021-04-24 09:41:42 +0800

    update to not let student cancel sessions

--- upcoming-app.php
+++ upcoming-app.php
@@ -48,7 +48,11 @@ if (isset($_POST['delete'])) {
                     <th scope="col">Time</th>
                     <th scope="col">Student</th>
                     <th scope="col">Comments</th>
-                    <th scope="col">Action</th>
+                    <?php
+                    if ($_SESSION['accountType'] != 'student') {
+                        echo '<th scope="col">Action</th>';
+                    }
+                    ?>
                 </tr>
                 </thead>
                 <tbody>
@@ -61,14 +65,17 @@ if (isset($_POST['delete'])) {
                     echo "<td>" . $appointment['student'] . "</td>";
                     echo "<td style=\"word-wrap: break-word;min-width: 100px;max-width: 100px;\">" .
                         $appointment['comments'] . "</td>";
-                    echo "<td><button class='btn btn-danger btn-block' 
+                    if ($_SESSION['accountType'] != 'student') {
+                        echo "<td><button class='btn btn-danger btn-block' 
                                     value=" . $appointment['id'] . " name='delete' 
                                     type='submit'>" . '<svg xmlns="http://www.w3.org/2000/svg" 
                                     width="16" height="16" fill="currentColor" class="bi bi-check" 
                                     viewBox="0 0 16 16"><path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 
                                     4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 
                                     3.473-4.425a.267.267 0 0 1 .02-.022z"/></svg>' . "</button>" . "</td>";
+                    }
                     echo "</tr>";
+
                 }
                 ?>
                 </tbody>
